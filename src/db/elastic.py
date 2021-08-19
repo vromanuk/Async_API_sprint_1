f0610719b8584ff76a6f7c4aae816e192389b885
Vlad Romaniuk <v.romaniuk@orderry.com>
2021-08-19 19:34:17 +0300

    Remove redundant comments

--- src/db/elastic.py
+++ src/db/elastic.py
@@ -3,6 +3,5 @@ from elasticsearch import AsyncElasticsearch
 es: AsyncElasticsearch = None
 
 
-# Функция понадобится при внедрении зависимостей
 async def get_elastic() -> AsyncElasticsearch:
     return es
--- src/db/redis.py
+++ src/db/redis.py
@@ -3,6 +3,5 @@ from aioredis import Redis
 redis: Redis = None
 
 
-# Функция понадобится при внедрении зависимостей
 async def get_redis() -> Redis:
     return redis

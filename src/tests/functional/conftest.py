8ee781fbe3eada96d09274c3dab78cd1c8a7b16d
Vlad Romaniuk <v.romaniuk@orderry.com>
2021-08-31 21:36:57 +0300

    Fix issue with strange behavior of event_loop and tests that were launched in suitcase

--- src/tests/functional/conftest.py
+++ src/tests/functional/conftest.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 from dataclasses import dataclass
 from pathlib import Path
@@ -32,40 +33,51 @@ class HTTPResponse:
         return self.status // 200 == 1
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="session")
 async def es_client(settings: TestSettings):
     client = AsyncElasticsearch(hosts=settings.es_host)
     yield client
     await client.close()
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 async def setup(settings: TestSettings):
     elastic.es = AsyncElasticsearch(hosts=[settings.es_host])
     redis.redis = await aioredis.create_redis_pool((settings.redis_host, settings.redis_port), minsize=10, maxsize=20)
     yield
 
 
-@pytest.fixture
-async def client(setup, settings: TestSettings):
-    async with AsyncClient(app=app, base_url=settings.base_url) as async_client:
-        yield async_client
-
-
-@pytest.fixture
+@pytest.fixture(scope="session")
 async def settings():
     return get_settings()
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 async def populate_es(es_client):
     movies_raw = load_params_from_json("fixtures/initial_data.json")
 
     await es_client.indices.create("movies")
 
+    body = []
     for movie in movies_raw:
-        await es_client.index(index="movies", id=movie["id"], body=movie)
+        body.append({"index": {"_id": movie["id"]}})
+        body.append(movie)
+
+    await es_client.bulk(index="movies", doc_type="doc", body=body)
 
     yield
 
     await es_client.indices.delete(index="movies")
+
+
+@pytest.fixture(scope="session")
+async def client(setup, settings: TestSettings, populate_es):
+    async with AsyncClient(app=app, base_url=settings.base_url) as async_client:
+        yield async_client
--- src/tests/functional/src/test_films.py
+++ src/tests/functional/src/test_films.py
@@ -1,12 +1,16 @@
+from asyncio import sleep
+
 import pytest
+from elasticsearch import NotFoundError
 from httpx import AsyncClient
 
 from src.models.film import Film, MovieType
 
 
 @pytest.mark.asyncio
-async def test_film_list(client: AsyncClient, populate_es):
+async def test_film_list(client: AsyncClient):
     film_list_url = "/films/"
+    await sleep(0.5)
     # Fetch data from elastic
     response = await client.get(film_list_url)
     resp_json = response.json()
@@ -25,7 +29,7 @@ async def test_film_list(client: AsyncClient, populate_es):
 
 
 @pytest.mark.asyncio
-async def test_film_details(client: AsyncClient, populate_es):
+async def test_film_details(client: AsyncClient):
     film_details_url = "/films/1"
     # Fetch data from elastic
     response = await client.get(film_details_url)
@@ -43,3 +47,48 @@ async def test_film_details(client: AsyncClient, populate_es):
     assert response.status_code == 200
     assert resp_json != []
     assert isinstance(Film.parse_obj(resp_json), Film)
+
+
+@pytest.mark.asyncio
+async def test_film_details_404(client: AsyncClient):
+    film_details_url = "/films/-1"
+    with pytest.raises(NotFoundError):
+        response = await client.get(film_details_url)
+        assert response.status_code == 404
+
+
+@pytest.mark.asyncio
+async def test_film_list_wrong_sort_field(client: AsyncClient):
+    film_list_url = "/films/?sort=None"
+    response = await client.get(film_list_url)
+    resp_json = response.json()
+    assert response.status_code == 422
+    assert "value is not a valid enumeration member" in resp_json["detail"][0]["msg"]
+
+
+@pytest.mark.asyncio
+async def test_film_list_wrong_order_field(client: AsyncClient):
+    film_list_url = "/films/?sort_order=None"
+    response = await client.get(film_list_url)
+    resp_json = response.json()
+    assert response.status_code == 422
+    assert "value is not a valid enumeration member" in resp_json["detail"][0]["msg"]
+
+
+@pytest.mark.asyncio
+async def test_film_list_limit(client: AsyncClient):
+    film_list_url = "/films/"
+    await sleep(0.5)
+    response = await client.get(f"{film_list_url}?page=1&limit=2")
+    resp_json = response.json()
+
+    assert response.status_code == 200
+    assert len(resp_json) == 2
+    assert resp_json[1]["title"] == "2046"
+
+    response = await client.get(f"{film_list_url}?page=2&limit=2")
+    resp_json = response.json()
+
+    assert response.status_code == 200
+    assert len(resp_json) == 2
+    assert resp_json[0]["title"] == "From Paris with Love"
